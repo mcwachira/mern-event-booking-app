b645d3030fdb6ba8917062fc2ff95bf2bdfeccf5
mcwachira <23430963+mcwachira@users.noreply.github.com>
2024-10-12 10:16:24 +0300

    installed antd for components and setup a theme Provider to customize the theme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
+import {Button , Input} from 'antd'
+import ThemeProvider from "./theme";
 
 function App(props) {
     return (
-        <div className="h-screen flex justify-center items-center bg-gray-500">
+        <ThemeProvider>
+            <div className="h-screen flex justify-center items-center  flex-col gap-5">
 
-            <h1 className=" text-[#fff] text-3xl font-bold leading-tight">
-                Home page
-            </h1>
-        </div>
+                <h1 className=" text-gray-500 text-3xl font-bold leading-tight">
+                    Home page
+                </h1>
+
+                <Button type="primary">
+                    Primary
+                </Button>
+                <Button type="default">
+                    Default
+                </Button>
+
+                <Input/>
+            </div>
+        </ThemeProvider>
+
     );
 }
 
