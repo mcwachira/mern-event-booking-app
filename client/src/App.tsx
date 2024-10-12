2aa5df12822131da4cc10ef360cc7e439995227b
mcwachira <23430963+mcwachira@users.noreply.github.com>
2024-10-12 09:56:40 +0300

    initialized app using bun and setup tailwindcss

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App(props) {
+    return (
+        <div className="h-screen flex justify-center items-center bg-gray-500">
+
+            <h1 className=" text-[#fff] text-3xl font-bold leading-tight">
+                Home page
+            </h1>
+        </div>
+    );
+}
+
+export default App;
